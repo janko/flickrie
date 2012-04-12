48b0a873d74abbe17995820fe56a8645fe859975
Janko Marohnić <janko.marohnic@gmail.com>
2012-04-12 02:29:21 +0200

    Make the example photo hash at the bottom of the file prettier

--- lib/photo.rb
+++ lib/photo.rb
@@ -106,40 +106,48 @@ end
 
 __END__
 
-{"id"=>"6913731566",
-   "secret"=>"23879c079a",
-   "server"=>"7130",
-   "farm"=>8,
-   "dateuploaded"=>"1333956611",
-   "isfavorite"=>0,
-   "license"=>"0",
-   "safety_level"=>"0",
-   "rotation"=>0,
-   "owner"=>
-    {"nsid"=>"67131352@N04",
-     "username"=>"Janko Marohnić",
-     "realname"=>"",
-     "location"=>"",
-     "iconserver"=>"0",
-     "iconfarm"=>0},
-   "title"=>{"_content"=>"6913664138_61ffb9c0d7_b"},
-   "description"=>{"_content"=>""},
-   "visibility"=>{"ispublic"=>1, "isfriend"=>0, "isfamily"=>0},
-   "dates"=>
-    {"posted"=>"1333956611",
-     "taken"=>"2012-04-09 00:30:11",
-     "takengranularity"=>"0",
-     "lastupdate"=>"1333956616"},
-   "views"=>"0",
-   "editability"=>{"cancomment"=>0, "canaddmeta"=>0},
-   "publiceditability"=>{"cancomment"=>1, "canaddmeta"=>0},
-   "usage"=>{"candownload"=>1, "canblog"=>0, "canprint"=>0, "canshare"=>0},
-   "comments"=>{"_content"=>"0"},
-   "notes"=>{"note"=>[]},
-   "people"=>{"haspeople"=>0},
-   "tags"=>{"tag"=>[]},
-   "urls"=>
-    {"url"=>
-      [{"type"=>"photopage",
-        "_content"=>"http://www.flickr.com/photos/67131352@N04/6913731566/"}]},
-   "media"=>"photo"}
+{
+  "id" => "6913731566",
+  "secret" => "23879c079a",
+  "server" => "7130",
+  "farm" => 8,
+  "dateuploaded" => "1333956611",
+  "isfavorite" => 0,
+  "license" => "0",
+  "safety_level" => "0",
+  "rotation" => 0,
+  "owner" => {
+    "nsid" => "67131352@N04",
+    "username" => "Janko Marohnić",
+    "realname" => "",
+    "location" => "",
+    "iconserver" => "0",
+    "iconfarm" => 0
+  },
+  "title" => {"_content" => "6913664138_61ffb9c0d7_b"},
+  "description" => {"_content" => ""},
+  "visibility" => {"ispublic" => 1, "isfriend" => 0, "isfamily" => 0},
+  "dates" => {
+    "posted" => "1333956611",
+    "taken" => "2012-04-09 00:30:11",
+    "takengranularity" => "0",
+    "lastupdate" => "1333956616"
+  },
+  "views" => "0",
+  "editability" => {"cancomment" => 0, "canaddmeta" => 0},
+  "publiceditability" => {"cancomment" => 1, "canaddmeta" => 0},
+  "usage" => {"candownload" => 1, "canblog" => 0, "canprint" => 0, "canshare" => 0},
+  "comments" => {"_content" => "0"},
+  "notes" => {"note" => []},
+  "people" => {"haspeople" => 0},
+  "tags" => {"tag" => []},
+  "urls "=> {
+    "url"=> [
+      {
+        "type"=>"photopage",
+       "_content"=>"http://www.flickr.com/photos/67131352@N04/6913731566/"
+      }
+    ]
+  },
+  "media"=>"photo"}
+}
