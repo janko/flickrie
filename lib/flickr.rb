33804b91b6180f359f1c168f85496f78808d55f5
Janko Marohnić <janko.marohnic@gmail.com>
2012-04-12 02:27:47 +0200

    Make finding user by email or username more intuitive

--- lib/flickr.rb
+++ lib/flickr.rb
@@ -13,21 +13,23 @@ module Flickr
       Photoset.new_collection(response.body['photosets']['photoset'])
     end
 
-    def find_user_by_email(email)
-      response = client.find_user_by_email(email)
-      response = client.get_user_info(response.body['user']['nsid'])
-      User.new(response.body['person'])
-    end
-
-    def find_user_by_username(username)
-      response = client.find_user_by_username(username)
-      response = client.get_user_info(response.body['user']['nsid'])
-      User.new(response.body['person'])
-    end
-
     def find_user_by_id(user_id)
       response = client.get_user_info(user_id)
       User.new(response.body['person'])
+    end
+
+    def get_user_id(options)
+      if options[:email].nil? and options[:username].nil?
+        raise ArgumentError, "'email' or 'username' must be present"
+      end
+
+      response = if options[:email]
+                   client.find_user_by_email(options[:email])
+                 else
+                   client.find_user_by_username(options[:username])
+                 end
+
+      response.body['user']['id']
     end
 
     def find_photoset_by_id(photoset_id)
