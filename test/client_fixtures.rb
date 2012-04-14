54512ffffa770cc0dfa0407cf5474e7521529398
Janko Marohnić <janko.marohnic@gmail.com>
2012-04-14 20:15:57 +0200

    Finish Flickr::Client test

---
+++ test/client_fixtures.rb
@@ -0,0 +1,263 @@
+# encoding: utf-8
+MEDIA_FROM_SET = {
+  "photoset"=>
+  {"id"=>"72157629409394888",
+   "primary"=>"6913663366",
+   "owner"=>"67131352@N04",
+   "ownername"=>"Janko Marohnić",
+   "photo"=>
+     {"id"=>"6913731566",
+      "secret"=>"23879c079a",
+      "server"=>"7130",
+      "farm"=>8,
+      "title"=>"6913664138_61ffb9c0d7_b",
+      "isprimary"=>"0",
+      "license"=>"0",
+      "dateupload"=>"1333956611",
+      "datetaken"=>"2012-04-09 00:30:11",
+      "datetakengranularity"=>"0",
+      "ownername"=>"Janko Marohnić",
+      "iconserver"=>"0",
+      "iconfarm"=>0,
+      "lastupdate"=>"1334347067",
+      "latitude"=>0,
+      "longitude"=>0,
+      "accuracy"=>0,
+      "context"=>0,
+      "tags"=>"",
+      "machine_tags"=>"",
+      "views"=>"1",
+      "media"=>"photo",
+      "media_status"=>"ready",
+      "pathalias"=>nil,
+      "url_sq"=> "http://farm8.staticflickr.com/7130/6913731566_23879c079a_s.jpg",
+      "height_sq"=>75,
+      "width_sq"=>75,
+      "url_q"=> "http://farm8.staticflickr.com/7130/6913731566_23879c079a_q.jpg",
+      "height_q"=>"150",
+      "width_q"=>"150",
+      "url_t"=> "http://farm8.staticflickr.com/7130/6913731566_23879c079a_t.jpg",
+      "height_t"=>"100",
+      "width_t"=>"75",
+      "url_s"=> "http://farm8.staticflickr.com/7130/6913731566_23879c079a_m.jpg",
+      "height_s"=>"240",
+      "width_s"=>"180",
+      "url_n"=> "http://farm8.staticflickr.com/7130/6913731566_23879c079a_n.jpg",
+      "height_n"=>"320",
+      "width_n"=>240,
+      "url_m"=>"http://farm8.staticflickr.com/7130/6913731566_23879c079a.jpg",
+      "height_m"=>"500",
+      "width_m"=>"375",
+      "url_z"=> "http://farm8.staticflickr.com/7130/6913731566_23879c079a_z.jpg",
+      "height_z"=>"640",
+      "width_z"=>"480",
+      "url_c"=> "http://farm8.staticflickr.com/7130/6913731566_23879c079a_c.jpg",
+      "height_c"=>"800",
+      "width_c"=>600,
+      "url_l"=> "http://farm8.staticflickr.com/7130/6913731566_23879c079a_b.jpg",
+      "height_l"=>"1024",
+      "width_l"=>"768"},
+   "page"=>1,
+   "per_page"=>500,
+   "perpage"=>500,
+   "pages"=>1,
+   "total"=>"3"},
+  "stat"=>"ok"
+}
+
+SETS_FROM_USER = {
+  "photosets"=>
+  {"page"=>1,
+   "pages"=>1,
+   "perpage"=>2,
+   "total"=>2,
+   "photoset"=>
+     {"id"=>"72157629443464020",
+      "primary"=>"6913731566",
+      "secret"=>"23879c079a",
+      "server"=>"7130",
+      "farm"=>8,
+      "photos"=>"1",
+      "videos"=>0,
+      "title"=>{"_content"=>"Bla"},
+      "description"=>{"_content"=>""},
+      "needs_interstitial"=>0,
+      "visibility_can_see_set"=>1,
+      "count_views"=>"0",
+      "count_comments"=>"0",
+      "can_comment"=>0,
+      "date_create"=>"1334331151",
+      "date_update"=>"1334331155"}},
+  "stat"=>"ok"
+}
+
+FIND_USER = {
+  "user"=>
+  {"id"=>"67131352@N04",
+   "nsid"=>"67131352@N04",
+   "username"=>{"_content"=>"Janko Marohnić"}},
+ "stat"=>"ok"
+}
+
+USER_INFO = {
+  "person"=>
+  {"id"=>"67131352@N04",
+   "nsid"=>"67131352@N04",
+   "ispro"=>0,
+   "iconserver"=>"0",
+   "iconfarm"=>0,
+   "path_alias"=>nil,
+   "username"=>{"_content"=>"Janko Marohnić"},
+   "realname"=>{"_content"=>""},
+   "location"=>{"_content"=>""},
+   "description"=>{"_content"=>""},
+   "photosurl"=>{"_content"=>"http://www.flickr.com/photos/67131352@N04/"},
+   "profileurl"=>{"_content"=>"http://www.flickr.com/people/67131352@N04/"},
+   "mobileurl"=>
+    {"_content"=>"http://m.flickr.com/photostream.gne?id=67099213"},
+   "photos"=>
+    {"firstdatetaken"=>{"_content"=>"2011-06-21 21:43:09"},
+     "firstdate"=>{"_content"=>"1333954416"},
+     "count"=>{"_content"=>3}}},
+ "stat"=>"ok"
+}
+
+SET_INFO = {
+  "photoset"=>
+  {"id"=>"72157629409394888",
+   "owner"=>"67131352@N04",
+   "primary"=>"6913663366",
+   "secret"=>"0c9fb32336",
+   "server"=>"5240",
+   "farm"=>6,
+   "photos"=>3,
+   "count_views"=>"0",
+   "count_comments"=>"1",
+   "count_photos"=>"2",
+   "count_videos"=>"1",
+   "title"=>{"_content"=>"rođendan"},
+   "description"=>{"_content"=>""},
+   "can_comment"=>0,
+   "date_create"=>"1333954490",
+   "date_update"=>"1334189604"},
+ "stat"=>"ok"
+}
+
+MEDIA_INFO = {
+  "photo"=>
+  {"id"=>"6923154272",
+   "secret"=>"5519fab554",
+   "server"=>"5279",
+   "farm"=>6,
+   "dateuploaded"=>"1334189525",
+   "isfavorite"=>0,
+   "license"=>"0",
+   "safety_level"=>"0",
+   "rotation"=>0,
+   "owner"=>
+    {"nsid"=>"67131352@N04",
+     "username"=>"Janko Marohnić",
+     "realname"=>"",
+     "location"=>"",
+     "iconserver"=>"0",
+     "iconfarm"=>0},
+   "title"=>{"_content"=>"David Belle - Canon commercial"},
+   "description"=>{"_content"=>""},
+   "visibility"=>{"ispublic"=>1, "isfriend"=>0, "isfamily"=>0},
+   "dates"=>
+    {"posted"=>"1334189525",
+     "taken"=>"2012-04-11 17:12:05",
+     "takengranularity"=>"0",
+     "lastupdate"=>"1334259651"},
+   "views"=>"1",
+   "editability"=>{"cancomment"=>0, "canaddmeta"=>0},
+   "publiceditability"=>{"cancomment"=>1, "canaddmeta"=>0},
+   "usage"=>{"candownload"=>1, "canblog"=>0, "canprint"=>0, "canshare"=>0},
+   "comments"=>{"_content"=>"0"},
+   "notes"=>{"note"=>[]},
+   "people"=>{"haspeople"=>0},
+   "tags"=>
+    {"tag"=>
+      [{"id"=>"67099213-6923154272-471",
+        "author"=>"67131352@N04",
+        "raw"=>"David",
+        "_content"=>"david",
+        "machine_tag"=>0},
+       {"id"=>"67099213-6923154272-18012",
+        "author"=>"67131352@N04",
+        "raw"=>"Belle",
+        "_content"=>"belle",
+        "machine_tag"=>0}]},
+   "location"=>
+    {"latitude"=>37.792608,
+     "longitude"=>-122.402672,
+     "accuracy"=>"14",
+     "context"=>"0",
+     "neighbourhood"=>
+      {"_content"=>"Financial District",
+       "place_id"=>"GddgqTpTUb8LgT93hw",
+       "woeid"=>"23512022"},
+     "locality"=>
+      {"_content"=>"San Francisco",
+       "place_id"=>"7.MJR8tTVrIO1EgB",
+       "woeid"=>"2487956"},
+     "county"=>
+      {"_content"=>"San Francisco",
+       "place_id"=>".7sOmlRQUL9nK.kMzA",
+       "woeid"=>"12587707"},
+     "region"=>
+      {"_content"=>"California",
+       "place_id"=>"NsbUWfBTUb4mbyVu",
+       "woeid"=>"2347563"},
+     "country"=>
+      {"_content"=>"United States",
+       "place_id"=>"nz.gsghTUb4c2WAecA",
+       "woeid"=>"23424977"},
+     "place_id"=>"GddgqTpTUb8LgT93hw",
+     "woeid"=>"23512022"},
+   "geoperms"=>{"ispublic"=>1, "iscontact"=>0, "isfriend"=>0, "isfamily"=>0},
+   "urls"=>
+    {"url"=>
+      [{"type"=>"photopage",
+        "_content"=>"http://www.flickr.com/photos/67131352@N04/6923154272/"}]},
+   "media"=>"video",
+   "video"=>
+    {"ready"=>1,
+     "failed"=>0,
+     "pending"=>0,
+     "duration"=>"34",
+     "width"=>"480",
+     "height"=>"360"}},
+ "stat"=>"ok"
+}
+
+LICENSES = {
+  "licenses"=>
+  {"license"=>
+    [{"id"=>"0", "name"=>"All Rights Reserved", "url"=>""},
+     {"id"=>"4",
+      "name"=>"Attribution License",
+      "url"=>"http://creativecommons.org/licenses/by/2.0/"},
+     {"id"=>"6",
+      "name"=>"Attribution-NoDerivs License",
+      "url"=>"http://creativecommons.org/licenses/by-nd/2.0/"},
+     {"id"=>"3",
+      "name"=>"Attribution-NonCommercial-NoDerivs License",
+      "url"=>"http://creativecommons.org/licenses/by-nc-nd/2.0/"},
+     {"id"=>"2",
+      "name"=>"Attribution-NonCommercial License",
+      "url"=>"http://creativecommons.org/licenses/by-nc/2.0/"},
+     {"id"=>"1",
+      "name"=>"Attribution-NonCommercial-ShareAlike License",
+      "url"=>"http://creativecommons.org/licenses/by-nc-sa/2.0/"},
+     {"id"=>"5",
+      "name"=>"Attribution-ShareAlike License",
+      "url"=>"http://creativecommons.org/licenses/by-sa/2.0/"},
+     {"id"=>"7",
+      "name"=>"No known copyright restrictions",
+      "url"=>"http://www.flickr.com/commons/usage/"},
+     {"id"=>"8",
+      "name"=>"United States Government Work",
+      "url"=>"http://www.usa.gov/copyright.shtml"}]},
+ "stat"=>"ok"
+}
--- test/client_test.rb
+++ test/client_test.rb
@@ -1,17 +1,66 @@
 # encoding: utf-8
 require 'test/unit'
 require 'flickr/client'
+require 'client_fixtures'
+
+class Hash
+  def looks_like?(other)
+    if keys.sort != other.keys.sort or
+       sort.map(&:last).map(&:class) != other.sort.map(&:last).map(&:class)
+      return false
+    end
+    map do |key, value|
+      if value.is_a?(self.class)
+        if not value.looks_like?(other[key])
+          return false
+        end
+      elsif value.is_a?(Array)
+        value.each_with_index do |el, index|
+          if el.is_a?(self.class)
+            if not el.looks_like?(other[key][index])
+              return false
+            end
+          end
+        end
+      end
+    end
+    return true
+  end
+end
 
 class ClientTest < Test::Unit::TestCase
   def setup
     Flickr.api_key = ENV['FLICKR_API_KEY']
+    @client = Flickr.client
   end
 
   def test_internals
     assert_equal ENV['FLICKR_API_KEY'], Flickr.api_key
-    assert_instance_of Flickr::Client, Flickr.client
+    assert_instance_of Flickr::Client, @client
   end
 
   def test_api_calls
+    #license,date_upload,date_taken,owner_name,icon_server,original_format,last_update,geo,tags,machine_tags,o_dims,views,media,path_alias,url_sq,url_q,url_t,url_s,url_n,url_m,url_z,url_c,url_l,url_o
+    all_possible_extras = %w[license date_upload date_taken owner_name
+      icon_server original_format last_update geo tags machine_tags
+      o_dims views media path_alias url_sq url_q url_t url_s url_n url_m
+      url_z url_c url_l url_o].join(',')
+    response_body = @client.media_from_set(72157629409394888,
+      :extras => all_possible_extras).body
+    response_body['photoset']['photo'] = response_body['photoset']['photo'].first
+    assert response_body.looks_like?(MEDIA_FROM_SET)
+
+    response_body = @client.sets_from_user("67131352@N04").body
+    response_body['photosets']['photoset'] = response_body['photosets']['photoset'].first
+    assert response_body.looks_like?(SETS_FROM_USER)
+
+    assert @client.find_user_by_email("janko.marohnic@gmail.com").body.looks_like?(FIND_USER)
+    assert @client.find_user_by_username("Janko Marohnić").body.looks_like?(FIND_USER)
+
+    assert @client.get_user_info("67131352@N04").body.looks_like?(USER_INFO)
+    assert @client.get_set_info(72157629409394888).body.looks_like?(SET_INFO)
+    assert @client.get_media_info(6923154272).body.looks_like?(MEDIA_INFO)
+
+    assert @client.get_licenses
   end
 end
