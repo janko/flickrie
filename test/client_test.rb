b4b9222f4831ae51d14b04b05caa30021d21a813
Janko Marohnić <janko.marohnic@gmail.com>
2012-04-14 18:59:49 +0200

    A real test for Flick::Client will follow

--- test/client_test.rb
+++ test/client_test.rb
@@ -7,18 +7,11 @@ class ClientTest < Test::Unit::TestCase
     Flickr.api_key = ENV['FLICKR_API_KEY']
   end
 
-  def test_api_key
+  def test_internals
     assert_equal ENV['FLICKR_API_KEY'], Flickr.api_key
+    assert_instance_of Flickr::Client, Flickr.client
   end
 
-  def test_methods_not_raising_an_exception
-    assert_nothing_raised(Flickr::Error) do
-      client = Flickr.client
-      client.photos_from_set('72157629409394888')
-      client.sets_from_user('67131352@N04')
-      client.find_user_by_email('janko.marohnic@gmail.com')
-      client.find_user_by_username('Janko Marohnić')
-      client.get_user_info('67131352@N04')
-    end
+  def test_api_calls
   end
 end
