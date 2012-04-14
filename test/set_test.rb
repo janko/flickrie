6009a0120873a69ea03e7b0252973c4f5ea75e2f
Janko Marohnić <janko.marohnic@gmail.com>
2012-04-14 19:00:34 +0200

    Finish Flickr::Set test

--- test/set_test.rb
+++ test/set_test.rb
@@ -9,39 +9,46 @@ class SetTest < Test::Unit::TestCase
     end
   end
 
-  HASH = {
-    'id' => '72157629409394888',
-    'owner' => '67131352@N04',
-    'primary' => '6913663366',
-    'secret' => '0c9fb32336',
-    'server' => '5240',
-    'farm' => 6,
-    'photos' => 2,
-    'count_views' => '0',
-    'count_comments' => '0',
-    'count_photos' => '2',
-    'count_videos' => 0,
-    'title' => {'_content' => 'rođendan'},
-    'description' => {'_content' => ''},
-    'can_comment' => 0,
-    'date_create' => '1333954490',
-    'date_update' => '1333956652'
-  }
-
   def test_attributes
-    set = Flickr::Set.public_new(HASH)
+    info_hash = {
+      'id' => '72157629409394888',
+      'owner' => '67131352@N04',
+      'primary' => '6913663366',
+      'secret' => '0c9fb32336',
+      'server' => '5240',
+      'farm' => 6,
+      'photos' => 3,
+      'count_views' => '0',
+      'count_comments' => '0',
+      'count_photos' => '2',
+      'count_videos' => 1,
+      'title' => {'_content' => 'rođendan'},
+      'description' => {'_content' => ''},
+      'can_comment' => 1,
+      'date_create' => '1333954490',
+      'date_update' => '1333956652'
+    }
+    set = Flickr::Set.public_new(info_hash)
 
     assert_equal 72157629409394888, set.id
-    assert_equal "67131352@N04", set.owner_id
-    assert_equal 2, set.photos_count
-    assert_equal 0, set.comments_count
-    assert_equal "rođendan", set.title
-    assert_equal "", set.description
-    assert_equal Time.at(1333954490), set.created_at
-    assert_equal Time.at(1333956652), set.updated_at
-    assert_equal HASH, set.flickr_hash
+    assert_equal '67131352@N04', set.owner.nsid
+    assert_equal 6913663366, set.primary_item_id
+    assert_equal '0c9fb32336', set.secret
+    assert_equal 6, set.farm
+    assert_equal 'rođendan', set.title
+    assert_equal '', set.description
 
-    Flickr.api_key = ENV['FLICKR_API_KEY']
-    assert_equal "67131352@N04", set.owner.id
+    assert_equal 3, set.items_count
+    assert_equal 2, set.photos_count
+    assert_equal 1, set.videos_count
+    assert_equal 0, set.comments_count
+    assert_equal 0, set.views_count
+
+    assert_instance_of Flickr::User, set.owner
+    assert set.can_comment?
+    refute set.url.empty?
+
+    assert_instance_of Time, set.created_at
+    assert_instance_of Time, set.updated_at
   end
 end
