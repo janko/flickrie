183ef8bf3fd2d8bbb9c824d7c60f0201065aef9f
Janko Marohnić <janko.marohnic@gmail.com>
2012-04-17 11:33:45 +0200

    Improve video's attributes to return nil when they have to

--- lib/flickr/video.rb
+++ lib/flickr/video.rb
@@ -4,14 +4,14 @@ module Flickr
   class Video
     include Media
 
-    def ready?;   @video['ready'].to_i == 1   end
-    def failed?;  @video['failed'].to_i == 1  end
-    def pending?; @video['pending'].to_i == 1 end
+    def ready?;   @video['ready'].to_i == 1 if @video['ready']     end
+    def failed?;  @video['failed'].to_i == 1 if @video['failed']   end
+    def pending?; @video['pending'].to_i == 1 if @video['pending'] end
 
-    def duration; @video['duration'].to_i end
+    def duration; @video['duration'].to_i if @video['duration'] end
 
-    def width;  @video['width'].to_i  end
-    def height; @video['height'].to_i end
+    def width;  @video['width'].to_i if @video['width']   end
+    def height; @video['height'].to_i if @video['height'] end
 
     def source_url;          @video['source_url']          end
     def download_url;        @video['download_url']        end
