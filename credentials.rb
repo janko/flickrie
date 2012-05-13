c1e3ded87f1169bb7f152159b3a574063768a106
Janko Marohnić <janko.marohnic@gmail.com>
2012-05-13 14:01:47 +0200

    add credentials.rb to version control

---
+++ credentials.rb
@@ -0,0 +1,4 @@
+Flickrie.api_key = ENV['FLICKR_API_KEY']
+Flickrie.shared_secret = ENV['FLICKR_SHARED_SECRET']
+Flickrie.access_token = ENV['FLICKR_ACCESS_TOKEN']
+Flickrie.access_secret = ENV['FLICKR_ACCESS_SECRET']
