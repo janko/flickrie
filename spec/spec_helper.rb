062cdf85cf932cd2ed290c3e6c5a147385b88e11
Janko Marohnić <janko.marohnic@gmail.com>
2012-05-14 18:46:36 +0200

    Move VCR cassettes to fixtures/ folder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ rescue LoadError
 end
 
 VCR.configure do |config|
-  config.cassette_library_dir = 'spec/vcr_cassettes'
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :faraday
   config.default_cassette_options = {
     :record => :new_episodes,
