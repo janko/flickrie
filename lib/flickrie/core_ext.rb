0d398ab55cd0f98754adf66622b6e5e42475d27f
Janko Marohnić <janko.marohnic@gmail.com>
2012-05-30 14:14:10 +0200

    Add Hash#except to core_ext

--- lib/flickrie/core_ext.rb
+++ lib/flickrie/core_ext.rb
@@ -1,13 +1,22 @@
 class Hash
-  def deep_merge(other_hash)
-    dup.deep_merge!(other_hash)
-  end
-
   def deep_merge!(other_hash)
     other_hash.each_pair do |k,v|
       tv = self[k]
       self[k] = tv.is_a?(Hash) && v.is_a?(Hash) ? tv.deep_merge(v) : v
     end
     self
+  end
+
+  def deep_merge(other_hash)
+    dup.deep_merge!(other_hash)
+  end
+
+  def except!(*keys)
+    keys.each { |key| delete(key) }
+    self
+  end
+
+  def except(*keys)
+    self.dup.except!(*keys)
   end
 end
